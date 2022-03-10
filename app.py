2d0fda2bb624cc9de87100de8a30196aa59c6986
Shishir Saxena <shishir98@live.com>
2022-03-10 15:31:16 +0530

    Changes#2

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
+import datetime
+
+from commands import wishMe, trigger_word, speak, takeCommand
 import subprocess
 import wolframalpha
-import pyttsx3
 import tkinter
 import json
 import random
 import operator
-import speech_recognition as sr
-import datetime
 import wikipedia
 import webbrowser
 import os
@@ -17,74 +17,12 @@ import smtplib
 import ctypes
 import time
 import requests
-import shutil
 from twilio.rest import Client
 # from client.textui import progress
 from ecapture import ecapture as ec
 from bs4 import BeautifulSoup
 import win32com.client as wincl
 from urllib.request import urlopen
-
-engine = pyttsx3.init('sapi5')
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
-
-trigger_word = ("patricia")
-def speak(audio):
-    engine.say(audio)
-    engine.runAndWait()
-
-
-def wishMe():
-    hour = int(datetime.datetime.now().hour)
-    if hour >= 0 and hour < 12:
-        speak("Good Morning Sir !")
-
-    elif hour >= 12 and hour < 18:
-        speak("Good Afternoon Sir !")
-
-    else:
-        speak("Good Evening Sir !")
-
-    speak("I am your Assistant")
-    speak(trigger_word)
-
-
-def username():
-    speak("What should i call you sir")
-    uname = takeCommand()
-    speak("Welcome Mister")
-    speak(uname)
-    columns = shutil.get_terminal_size().columns
-
-    print("#####################".center(columns))
-    print("Welcome Mr.", uname.center(columns))
-    print("#####################".center(columns))
-
-    speak("How can i Help you, Sir")
-
-
-def takeCommand():
-    r = sr.Recognizer()
-
-    with sr.Microphone(device_index=2) as source:
-
-        print("Listening...")
-        r.pause_threshold = 1
-        audio = r.listen(source)
-
-    try:
-        print("Recognizing...")
-        query = r.recognize_google(audio, language='en-in')
-        print(f"User said: {query}\n")
-
-    except Exception as e:
-        print(e)
-        print("Unable to Recognize your voice.")
-        return "None"
-
-    return query
-
 
 if __name__ == '__main__':
     clear = lambda: os.system('cls')
@@ -139,19 +77,19 @@ if __name__ == '__main__':
                 speak(f"Sir, the time is {strTime}")
 
             elif 'open opera' in query:
-                codePath = r"C:\\Users\\GAURAV\\AppData\\Local\\Programs\\Opera\\launcher.exe"
+                codePath = r"xyz"
                 os.startfile(codePath)
 
-            elif 'email to gaurav' in query:
-                try:
-                    speak("What should I say?")
-                    content = takeCommand()
-                    to = "Receiver email address"
-                    sendEmail(to, content)
-                    speak("Email has been sent !")
-                except Exception as e:
-                    print(e)
-                    speak("I am not able to send this email")
+            # elif 'email to gaurav' in query:
+            #     try:
+            #         speak("What should I say?")
+            #         content = takeCommand()
+            #         to = "Receiver email address"
+            #         sendEmail(to, content)
+            #         speak("Email has been sent !")
+            #     except Exception as e:
+            #         print(e)
+            #         speak("I am not able to send this email")
 
             # elif 'send a mail' in query:
             #     try:
---
+++ commands.py
@@ -0,0 +1,70 @@
+
+import pyttsx3
+
+import speech_recognition as sr
+import datetime
+
+import shutil
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+trigger_word = ("patricia")
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour >= 0 and hour < 12:
+        speak("Good Morning Sir !")
+
+    elif hour >= 12 and hour < 18:
+        speak("Good Afternoon Sir !")
+
+    else:
+        speak("Good Evening Sir !")
+
+    speak("I am your Assistant")
+    speak(trigger_word)
+
+
+def username():
+    speak("What should i call you sir")
+    uname = takeCommand()
+    speak("Welcome Mister")
+    speak(uname)
+    columns = shutil.get_terminal_size().columns
+
+    print("#####################".center(columns))
+    print("Welcome Mr.", uname.center(columns))
+    print("#####################".center(columns))
+
+    speak("How can i Help you, Sir")
+
+
+def takeCommand():
+    r = sr.Recognizer()
+
+    with sr.Microphone(device_index=2) as source:
+
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        print(e)
+        print("Unable to Recognize your voice.")
+        return "None"
+
+    return query
+
+
+#
